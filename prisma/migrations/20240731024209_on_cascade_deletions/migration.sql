e6b731652c4624e407ff1f0d05a3219017049143
Solo <juanoliugomezplatero@gmail.com>
2024-08-02 13:21:11 -0300

    edited prisma schema and generated a migration to allow deletions on cascade

---
+++ prisma/migrations/20240731024209_on_cascade_deletions/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "Chat" DROP CONSTRAINT "Chat_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Message" DROP CONSTRAINT "Message_chatId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;

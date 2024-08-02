327dd0b85d543e0f16bc3def6fb21d81db1c1fad
Solo <juanoliugomezplatero@gmail.com>
2024-08-02 13:56:41 -0300

    refactor and changes on message component

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,50 +1,48 @@
-import { useSession } from "next-auth/react";
+"use client";
+
 import Image from "next/image";
 
-export type MessageProps = {
-  id: string;
-  username: string;
-  avatar?: string;
-  body: string;
-  likes: number;
-  createdAt: string;
-};
-
 interface Props {
-  message: MessageProps;
+  id?: number;
+  isAuthorMsg: boolean;
+  actionMsg?: string;
+  username: string;
+  avatar: string;
+  content: string;
+  createdAt?: Date;
 }
 
-export const Message = ({ message }: Props) => {
-  const { data: session } = useSession();
-
-  const isAuthorMsg = message.username === session?.user?.name;
-
+export const Message = ({
+  avatar,
+  content,
+  isAuthorMsg,
+  actionMsg,
+  username,
+}: Props) => {
   return (
     <div
       className={`flex relative space-x-1 gap-2 ${
         isAuthorMsg ? "flex-row-reverse space-x-reverse" : "flex-row"
       }`}
     >
-      {message?.avatar && (
-        <div className="w-12 h-12 overflow-hidden flex-shrink-0 rounded">
-          <Image
-            width={50}
-            height={50}
-            src={message.avatar}
-            alt={message.username}
-          />
+      <div className="flex flex-col gap-2">
+        {actionMsg && <span>{actionMsg}</span>}
+        <div className="flex flex-row gap-2.5">
+          <div className="w-12 h-12 overflow-hidden flex-shrink-0 rounded">
+            <Image width={50} height={50} src={avatar} alt={username} />
+          </div>
+          <span
+            className={`inline-flex rounded space-x-2 items-start p-3 text-white ${
+              isAuthorMsg ? "bg-emerald-700" : "bg-neutral-700"
+            } `}
+          >
+            {isAuthorMsg && (
+              <span className="font-bold">{username}:&nbsp;</span>
+            )}
+            {content}
+          </span>
         </div>
-      )}
-      <span
-        className={`inline-flex rounded space-x-2 items-start p-3 text-white ${
-          isAuthorMsg ? "bg-emerald-700" : "bg-neutral-700"
-        } `}
-      >
-        {isAuthorMsg && (
-          <span className="font-bold">{message.username}:&nbsp;</span>
-        )}
-        {message.body}
-      </span>
+      </div>
     </div>
   );
 };

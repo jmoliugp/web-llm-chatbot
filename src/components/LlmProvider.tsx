82ff9bffefaa95c8828344cd84bcdced13f9ceed
Solo <juanoliugomezplatero@gmail.com>
2024-08-02 15:06:16 -0300

    llm chatbot on reply action with streaming

--- src/components/LlmProvider.tsx
+++ src/components/LlmProvider.tsx
@@ -1,7 +1,21 @@
 "use client";
 
-import { CreateMLCEngine, InitProgressReport } from "@mlc-ai/web-llm";
-import React, { createContext, ReactNode, useContext, useState } from "react";
+import { ChatMessage, ChatMessages } from "@/actions/chat";
+import { QueryKey } from "@/networking/types";
+import {
+  ChatCompletionMessageParam,
+  CreateMLCEngine,
+  InitProgressReport,
+  MLCEngine,
+} from "@mlc-ai/web-llm";
+import { useQueryClient } from "@tanstack/react-query";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 enum LlmModel {
   Llama3 = "Llama-3.1-8B-Instruct-q4f32_1-MLC",
@@ -10,6 +24,8 @@ enum LlmModel {
 interface Context {
   initWebLlm: () => Promise<void>;
   llmProgressReport?: InitProgressReport;
+  onReply: (chatId: number) => Promise<void>;
+  reply: string | undefined;
 }
 
 interface Props {
@@ -20,9 +36,12 @@ const WebLlmContext = createContext<Context>({} as Context);
 export const useWebLlmContext = () => useContext(WebLlmContext);
 
 export const WebLlmProvider: React.FC<Props> = ({ children }) => {
+  const [engine, setEngine] = useState<MLCEngine | undefined>();
+  const [reply, setReply] = useState<string | undefined>();
   const [llmProgressReport, setLlmProgressReport] = useState<
     InitProgressReport | undefined
   >(undefined);
+  const queryClient = useQueryClient();
 
   const initWebLlm = async () => {
     const engine = await CreateMLCEngine(LlmModel.Llama3, {
@@ -34,10 +53,48 @@ export const WebLlmProvider: React.FC<Props> = ({ children }) => {
         }
       },
     });
+    setEngine(engine);
+  };
+  useEffect(() => {
+    initWebLlm();
+  }, []);
+
+  const onReply = async (chatId: number) => {
+    if (!engine) return;
+
+    const rawMessages: ChatMessage[] =
+      queryClient.getQueryData<ChatMessages>([QueryKey.ChatMessages, chatId])
+        ?.messages ?? [];
+    const messages: ChatCompletionMessageParam[] = rawMessages.map((rawMsg) => {
+      return {
+        role: rawMsg.sender === "LLM" ? "system" : "user",
+        content: rawMsg.content,
+      };
+    });
+
+    const chunks = await engine.chat.completions.create({
+      messages,
+      temperature: 1,
+      stream: true,
+      stream_options: { include_usage: true },
+    });
+
+    setReply("");
+    for await (const chunk of chunks) {
+      const msgChunk = chunk.choices[0]?.delta.content;
+      if (!msgChunk) continue;
+
+      setReply((prev) => prev + msgChunk);
+    }
+
+    const fullReply = await engine.getMessage();
+    console.log(fullReply);
   };
 
   return (
-    <WebLlmContext.Provider value={{ initWebLlm, llmProgressReport }}>
+    <WebLlmContext.Provider
+      value={{ initWebLlm, llmProgressReport, onReply, reply }}
+    >
       {children}
     </WebLlmContext.Provider>
   );

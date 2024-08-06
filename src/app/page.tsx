bda7f6fac78c8eb94529a749a8da7e92b49fdc27
Solo <juanoliugomezplatero@gmail.com>
2024-08-06 17:12:08 -0300

    removes no longer used code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,10 @@ import { getServerSession } from "next-auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
-
   if (!session) {
     return (
       <div className="flex flex-col h-screen w-screen">
         <Header />
-
         <div className="h-full w-full flex items-center justify-center">
           <p className="text-lg md:text-2xl text-white">
             Sign in to join the chat!
@@ -23,32 +21,17 @@ export default async function Home() {
   }
 
   const chat = await getLatestChat(session.user.id);
-
   return (
     <div className="flex flex-col h-screen w-screen">
       <Header />
-      {session ? (
-        <>
-          <div className="flex-1  p-6 ">
-            <div className="max-w-4xl mx-auto">
-              <div className="flex justify-between items-center">
-                <MessageList chat={chat} user={session.user} />
-              </div>
-            </div>
-          </div>
-          <div className="p-6 bg-white/5 border-t border-neutral-700">
-            <div className="max-w-4xl mx-auto">
-              <NewMessageForm chat={chat} />
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="h-full w-full flex items-center justify-center">
-          <p className="text-lg md:text-2xl text-white">
-            Sign in to join the chat!
-          </p>
+
+      <MessageList chat={chat} user={session.user} />
+
+      <div className="p-6 bg-white/5 border-t border-neutral-700 ">
+        <div className="max-w-4xl mx-auto">
+          <NewMessageForm chat={chat} />
         </div>
-      )}
+      </div>
     </div>
   );
 }
